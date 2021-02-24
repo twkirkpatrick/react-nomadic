21c34673d6b363223f3decfc69a44337019561ea
Tanner Kirkpatrick <twk4491@gmail.com>
2021-02-24 13:41:23 -0500

    created navbar component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import Navigation from "./components/layout/Navigation";
 import { connect } from "react-redux";
 import { getLocations } from "./redux/actions/locationActions";
 
 function App({ getLocations }) {
   return (
     <div className="App">
+      <Navigation />
       <button onClick={getLocations}>get locations</button>
     </div>
   );
---
+++ client/src/components/layout/Navigation.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+
+const Navigation = () => {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="#home">Nomadic</Navbar.Brand>
+      </Navbar>
+    </>
+  );
+};
+
+export default Navigation;
