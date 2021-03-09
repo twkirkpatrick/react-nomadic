e0a75f69898f7d64174c2c0a6bd8d3772c29aa59
Tanner Kirkpatrick <twk4491@gmail.com>
2021-03-09 08:57:10 -0500

    media query added to map for responiveness

--- client/src/components/locations/LocationField.js
+++ client/src/components/locations/LocationField.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const LocationField = () => {
   return (
-    <div style={{ backgroundColor: "white", height: "100vh" }}>
+    <div
+      style={{ backgroundColor: "white", height: "50vh", textAlign: "center" }}
+    >
       <h1>Dublin, Ireland</h1>
       <h3>12/14/2021</h3>
     </div>
